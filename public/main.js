4ae5d87f9e40b15e698b55648168303a1888bdf9
Kenny <kawhyte@gmail.com>
2020-02-21 17:00:13 -0800

    updated

--- public/main.js
+++ public/main.js
@@ -235,9 +235,9 @@ function generateHTML(data, index) {
 
 
 
-// fetch("http://localhost:8887/track", {
+ fetch("http://localhost:8887/track", {
 // fetch("http://localhost:9000/index", {
-fetch('/.netlify/functions/index', {
+//fetch('/.netlify/functions/index', {
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json"
